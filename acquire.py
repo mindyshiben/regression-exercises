6e55df471d7855e39eee4b532fc8777fc02bce5a
Mindy Shiben <mindy.shiben@gmail.com>
2022-06-03 09:11:16 -0500

    file_changes

--- acquire.py
+++ acquire.py
@@ -11,7 +11,7 @@ def get_zillow_data():
     
     else:
         sql = '''
-        SELECT bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips, assessmentyear, landtaxvaluedollarcnt, lotsizesquarefeet, latitude, longitude
+        SELECT parcelid, bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips, assessmentyear, landtaxvaluedollarcnt, lotsizesquarefeet
         FROM properties_2017
         WHERE propertylandusetypeid = 261;
         '''
